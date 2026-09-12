bb4dd7644d4b463e6ef560e7c2c95b39fcc8b0c4
Maksym Korniichuk <maxsik1337yt@gmail.com>
2026-09-12 12:00:53 +0300

    added main file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <random>
+
+int randomNumber(int min, int max) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(min, max);
+
+    return dist(gen);
+}
+
+int main() {
+    int number = randomNumber(1, 100);
+
+    std::cout << number << std::endl;
+
+    return 0;
+}
